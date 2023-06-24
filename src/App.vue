125ae2c7e5b7427c74ca6086703af10afbcc8b6c
John Corrigan <jcorrigan322@gmail.com>
2023-06-23 17:28:00 -0700

    tailwind and firebase setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { db } from './firebase'
+import { collection, getDocs } from "firebase/firestore"; 
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+  const data = doc.data()
+  console.log(data)
+});
+
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <main class="min-h-screen min-w-screen flex justify-center items-center"> 
+  <h1 class="text-blue-500 font-bold text-6xl">mr poopy</h1>
+</main>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
