95a11369f306b1873c8fa7f0399de0c96857f02c
John Corrigan <jcorrigan322@gmail.com>
2023-06-27 18:53:24 -0700

    fix vercel deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,6 +41,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const isAuthenticated = auth.currentUser; // Check if user is authenticated
   console.log("test", auth.currentUser);
+  console.log(from);
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     // If the route requires authentication
     if (isAuthenticated) {
